fcf329eee6dc08b511b8aeb5e4c3dbd3d225e053
repogado1 <repogado@davao-water.gov.ph>
2025-07-20 12:48:40 +0800

    update

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase";
 import { format } from "date-fns";
